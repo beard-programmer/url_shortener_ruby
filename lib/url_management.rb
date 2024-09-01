d3a8696f6d66688534191878ac65f3a024eeb41b
victorshinkevich <supostatekx@gmail.com>
2024-08-31 23:16:01 -0400

    basic encode api

---
+++ lib/url_management.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module UrlManagement
+  require_relative './result'
+  require_relative './url_management/encode_api'
+  require_relative './url_management/api_schema_middleware'
+end
---
+++ lib/url_management/encode_api_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'rspec'
+require 'rack/test'
+require_relative './encode_api'
+
+RSpec.describe UrlManagement::EncodeApi do
+  include Rack::Test::Methods
+
+  def app
+    UrlManagement::EncodeApi.new
+  end
+
+  describe "POST /encode" do
+    context "with valid input" do
+      let(:valid_json) { { url: "https://example.com" }.to_json }
+
+      it "returns status 200" do
+        post '/encode', valid_json, { "CONTENT_TYPE" => "application/json" }
+        expect(last_response.status).to eq(200)
+      end
+
+      it "returns a short_url in the response" do
+        post '/encode', valid_json, { "CONTENT_TYPE" => "application/json" }
+        response_data = JSON.parse(last_response.body)
+        expect(response_data["short_url"]).to be_a(String)
+        expect(response_data["short_url"]).to match(%r{https://short\.est/\h{12}})
+      end
+
+      it "returns the original url in the response" do
+        post '/encode', valid_json, { "CONTENT_TYPE" => "application/json" }
+        response_data = JSON.parse(last_response.body)
+        expect(response_data["url"]).to eq("https://example.com")
+      end
+    end
+
+    context "with invalid input" do
+      let(:invalid_json) { { url: "short" }.to_json }
+
+      it "returns status 400" do
+        post '/encode', invalid_json, { "CONTENT_TYPE" => "application/json" }
+        expect(last_response.status).to eq(400)
+      end
+
+      it "returns an error message in the response" do
+        post '/encode', invalid_json, { "CONTENT_TYPE" => "application/json" }
+        response_data = JSON.parse(last_response.body)
+        expect(response_data["error"]).to include("The property '#/url' was not of a minimum string length of 10")
+      end
+    end
+
+    context "with missing url key" do
+      let(:missing_key_json) { {}.to_json }
+
+      it "returns status 400" do
+        post '/encode', missing_key_json, { "CONTENT_TYPE" => "application/json" }
+        expect(last_response.status).to eq(400)
+      end
+
+      it "returns an error message in the response" do
+        post '/encode', missing_key_json, { "CONTENT_TYPE" => "application/json" }
+        response_data = JSON.parse(last_response.body)
+
+        expect(response_data["error"]).to include("The property '#/' did not contain a required property of 'url'")
+      end
+    end
+  end
+end
