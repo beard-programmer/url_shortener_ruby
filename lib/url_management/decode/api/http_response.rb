0a9cf40a67e880f63c40585c532dacacca614460
victorshinkevich <supostatekx@gmail.com>
2024-09-03 14:56:06 -0400

    http response

---
+++ lib/url_management/decode/api/http_response.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'json'
+require_relative '../../../common/result'
+require_relative '../../decode'
+require_relative '../errors'
+
+module UrlManagement
+  module Decode
+    module Api
+      class HttpResponse
+        private_class_method :new
+        attr_reader :http_status_code, :body
+
+        def self.from_decode_result(result)
+          case result
+          in Result::Ok[UrlManagement::Decode::DecodedUrlWasFound[url:, short_url:]]
+            new(200, { url:, short_url: }.to_json)
+          in Result::Ok[UrlManagement::Decode::DecodedUrlWasNotFound[short_url]]
+            new(
+              422,
+              { error: { code: 'DecodedUrlWasNotFound',
+                         message: "Not found original url for short: #{short_url}" } }.to_json
+            )
+          in Result::Err[UrlManagement::Decode::ValidationError => e]
+            new(400, { error: { code: 'ValidationError', message: e } }.to_json)
+          in Result::Err[UrlManagement::Decode::InfrastructureError => e]
+            new(500, { error: { code: 'InfrastructureError', message: e } }.to_json)
+          else raise 'Unexpected decode result'
+          end
+        end
+
+        def initialize(http_status_code, body)
+          @http_status_code = http_status_code
+          @body = body
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/url_management/decode/api/http_response_spec.rb
@@ -0,0 +1,76 @@
+require_relative '../../../../spec_helper'
+require_relative '../../../../../lib/url_management/decode/api/http_response'
+
+RSpec.describe UrlManagement::Decode::Api::HttpResponse do
+  describe '.from_decode_result' do
+    let(:found_url) { "https://example.com" }
+    let(:short_url) { "https://short.url" }
+
+    context 'when the result is Ok with DecodedUrlWasFound' do
+      let(:result) { Result.ok(UrlManagement::Decode::DecodedUrlWasFound.new(found_url, short_url)) }
+
+      it 'returns a 200 status with the correct body' do
+        response = described_class.from_decode_result(result)
+        expect(response.http_status_code).to eq(200)
+        expect(response.body).to eq({ url: found_url, short_url: }.to_json)
+      end
+    end
+
+    context 'when the result is Ok with DecodedUrlWasNotFound' do
+      let(:result) { Result.ok(UrlManagement::Decode::DecodedUrlWasNotFound.new(short_url)) }
+
+      it 'returns a 422 status with an error message' do
+        response = described_class.from_decode_result(result)
+        expect(response.http_status_code).to eq(422)
+        expect(response.body).to eq({
+          error: {
+            code: 'DecodedUrlWasNotFound',
+            message: "Not found original url for short: #{short_url}"
+          }
+        }.to_json)
+      end
+    end
+
+    context 'when the result is Err with ValidationError' do
+      let(:error_message) { "Invalid URL format" }
+      let(:result) { Result.err UrlManagement::Decode::ValidationError.new(error_message) }
+
+      it 'returns a 400 status with an error message' do
+        response = described_class.from_decode_result(result)
+        expect(response.http_status_code).to eq(400)
+        expect(response.body).to eq({
+          error: {
+            code: 'ValidationError',
+            message: error_message
+          }
+        }.to_json)
+      end
+    end
+
+    context 'when the result is Err with InfrastructureError' do
+      let(:error_message) { "Database connection failed" }
+      let(:result) { Result.err(UrlManagement::Decode::InfrastructureError.new(error_message)) }
+
+      it 'returns a 500 status with an error message' do
+        response = described_class.from_decode_result(result)
+        expect(response.http_status_code).to eq(500)
+        expect(response.body).to eq({
+          error: {
+            code: 'InfrastructureError',
+            message: error_message
+          }
+        }.to_json)
+      end
+    end
+
+    context 'when the result is unexpected' do
+      let(:unexpected_result) { "Unexpected result" }
+
+      it 'raises an error' do
+        expect do
+          described_class.from_decode_result(unexpected_result)
+        end.to raise_error('Unexpected decode result')
+      end
+    end
+  end
+end
