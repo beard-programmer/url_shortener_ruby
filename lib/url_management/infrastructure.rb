bbdfa3222309141d2b026a9f5f50bf561d78dbbc
victorshinkevich <supostatekx@gmail.com>
2024-09-06 15:23:26 -0400

    rm unused require

--- lib/url_management/infrastructure.rb
+++ lib/url_management/infrastructure.rb
@@ -4,6 +4,5 @@ module UrlManagement
   module Infrastructure
     require_relative './infrastructure/parse_url_string'
     require_relative './infrastructure/codec_base58'
-    require_relative './infrastructure/event_publisher'
   end
 end
