4c599d372b3a1568ba10d41c96f05ccfa2b6abed
victorshinkevich <supostatekx@gmail.com>
2024-09-06 15:22:40 -0400

    puma config

---
+++ config/puma.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+setup = {
+  env: ENV['APP_ENV'] || 'development',
+  port: (ENV['PORT'] || 4567).to_i,
+  min_threads: (ENV['PUMA_MIN_THREADS'] || 16).to_i,
+  max_threads: (ENV['PUMA_MAX_THREADS'] || 32).to_i,
+  workers: (ENV['PUMA_WORKERS'] || 10).to_i
+}
+
+port        setup[:port]
+environment setup[:env]
+workers     setup[:workers]
+threads     setup[:min_threads], setup[:max_threads]
+
+preload_app!
+
+before_fork do
+  UrlManagement::Api.db.disconnect
+end
+
+on_worker_boot do
+  UrlManagement::Api.db.disconnect
+end
